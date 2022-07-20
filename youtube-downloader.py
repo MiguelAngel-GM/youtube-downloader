d1b4bed1f8f7c8715e062f5cf5e2afcd2c47644a
MiguelAngel-GM <90727190+MiguelAngel-GM@users.noreply.github.com>
2022-07-20 14:33:57 +0200

    Fixed mp4 to mp3 conversion using ffmpeg

--- youtube-downloader.py
+++ youtube-downloader.py
@@ -1,5 +1,6 @@
 from pytube import YouTube, Playlist
 import os
+import subprocess
 
 again=True
 
@@ -19,18 +20,24 @@ def downloadAudio(url, path='', list_mode=False):
     print(f"Va a descargarse el archivo {yt.title}.mp3 ({size} mb)")
     
     if not list_mode:
-        path=input("En que ruta quieres guardar tu descarga? (Si se deja en blanco, la descarga se guardara en el directorio donde se encuente ubicado el programa) --> ")
+        path=input("En que ruta quieres guardar tu descarga? (Si se deja en blanco, la descarga se guardara en el directorio donde se encuente ubicado el programa)\n> ")
         if (path!='' and path[-1]!='/'): path+='/'
+    
+    ########################### ARREGLAR ESTO #######################################
     for char in ["'", "á", "é", "í", "ó", "ú", 'ñ', 'º', 'ª']:  #si uno de estos caracteres esta en el titulo del video, hay que modificarlo para que no produzca errores
         if char in yt.title:
             title=''    # xdd
         else: title=yt.title
+    #################################################################################
 
     try:
-        audio_stream.download(output_path=path, filename=title)
-        file=path+title
-        file_mp3=path+title+'.mp3'
-        os.rename(file, file_mp3)   #el archivo descargado se convierte a mp3 (pytube descarga en mp4)
+        print("Descargando el archivo...")
+        file = path + title + '.mp4'
+        audio_stream.download(output_path=path, filename=file)
+        file_mp3 = path + title +'.mp3'
+        print("Realizando conversion a mp3...")
+        subprocess.run(["ffmpeg", "-i", file, file_mp3])    #el archivo descargado se convierte a mp3 (pytube descarga en mp4)
+        os.remove(file)
         print("Hecho!")
     except:
         print("Error en la descarga, revisa que los parametros sean correctos y que tienes conexion a internet.\n\n")
@@ -56,8 +63,8 @@ def downloadVideo(url, path='', res='',list_mode=False):
     if not list_mode:
         res=[stream.resolution for stream in video_streams]
         print(f"Las resoluciones disponibles son {res}")
-        res=input("Elige resolucion entre las disponibles --> ")
-        path=input("En que ruta quieres guardar tu descarga? (Si se deja en blanco, la descarga se guardara en el directorio donde se encuente ubicado el programa) --> ")
+        res=input("Elige resolucion entre las disponibles\n> ")
+        path=input("En que ruta quieres guardar tu descarga? (Si se deja en blanco, la descarga se guardara en el directorio donde se encuente ubicado el programa)\n> ")
         if (path!='' and path[-1]!='/'): path+='/'
 
     try:
@@ -85,14 +92,14 @@ def handleList(url, mode):
         repeat()
         return
 
-    list_path=input("En que ruta quieres guardar tu descarga? (Si se deja en blanco, la descarga se guardara en el directorio donde se encuente ubicado el programa) --> ")
+    list_path=input("En que ruta quieres guardar tu descarga? (Si se deja en blanco, la descarga se guardara en el directorio donde se encuente ubicado el programa)\n> ")
     if (list_path!='' and list_path[-1]!='/'): list_path+='/'
 
     if mode.lower()=='audio':
         for video in pl.videos:
             downloadAudio(video, list_path, list_mode=True)
     elif mode.lower()=='video':
-        list_res=input("A que resolucion quieres descargar los videos de la lista? (720p max) --> ")
+        list_res=input("A que resolucion quieres descargar los videos de la lista? (720p max)\n> ")
         for video in pl.videos:
             downloadVideo(video, list_path, list_res, list_mode=True)
 
@@ -119,11 +126,11 @@ def main():
     print("Hola :)")
     global again
     while again==True:
-        option=input("Quieres descargar un video o una lista? --> ")
+        option=input("Quieres descargar un video o una lista?\n> ")
         
         if option.lower()=='video':
-            url=input("Introduce el enlace al video que quieres descargar --> ")
-            mode=input("Quieres descargar el video completo o solo audio? (Escribe audio o video) --> ")
+            url=input("Introduce el enlace al video que quieres descargar\n> ")
+            mode=input("Quieres descargar el video completo o solo audio? (Escribe audio o video)\n> ")
 
             if mode.lower()=='audio': downloadAudio(url)
             elif mode.lower()=='video': downloadVideo(url)
@@ -131,8 +138,8 @@ def main():
 
         
         elif option.lower()=='lista':
-            url=input("Introduce el enlace a la lista que quieres descargar --> ")
-            mode=input("Quieres descargar los videos completos o solo su audio? (Escribe audio o video) --> ")
+            url=input("Introduce el enlace a la lista que quieres descargar\n> ")
+            mode=input("Quieres descargar los videos completos o solo su audio? (Escribe audio o video)\n> ")
 
             if mode.lower() not in ['audio', 'video']: print("Opcion invalida, solo son validas las opciones video y audio, prueba otra vez")        
             else: handleList(url, mode)
